a217665cc5ca0234324ea11c80275c605ca7472d
jordan hellsent <jordanhellsent@gmail.com>
2026-01-16 16:58:16 +0000

    File changes

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDKfGpwH8_QvJpbIpByaZUSYRLD_zXqYt0",
+  authDomain: "studio-2287473014-e93e4.firebaseapp.com",
+  projectId: "studio-2287473014-e93e4",
+  storageBucket: "studio-2287473014-e93e4.appspot.com",
+  messagingSenderId: "1063982704712",
+  appId: "1:1063982704712:web:42fbb933250b132db1107d",
 };
 
 // Initialize Firebase
