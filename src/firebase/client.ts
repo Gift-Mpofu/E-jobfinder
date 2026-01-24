e0255b7fba4c703678b8168df2e8536c7fd2142d
jordan hellsent <jordanhellsent@gmail.com>
2026-01-24 10:14:09 +0000

    File changes

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig: FirebaseOptions = {
+  projectId: "studio-2287473014-e93e4",
+  appId: "1:1063982704712:web:42fbb933250b132db1107d",
   apiKey: "AIzaSyDKfGpwH8_QvJpbIpByaZUSYRLD_zXqYt0",
   authDomain: "studio-2287473014-e93e4.firebaseapp.com",
-  projectId: "studio-2287473014-e93e4",
   storageBucket: "studio-2287473014-e93e4.appspot.com",
   messagingSenderId: "1063982704712",
-  appId: "1:1063982704712:web:42fbb933250b132db1107d",
 };
 
 // Initialize Firebase
